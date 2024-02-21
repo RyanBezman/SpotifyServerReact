e211a71bc3e60ba747ed8349416c3ef15d15b60a
Terence Bezman <terencebezman@gmail.com>
2024-02-20 20:47:00 -0500

    revalidate path after adding song to liked playlist

--- actions/actions.tsx
+++ actions/actions.tsx
@@ -1,42 +1,33 @@
 "use server";
-import { PrismaClient, Song } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
+import { revalidatePath } from "next/cache";
 
 const prisma = new PrismaClient();
 
 export const addToLiked = async (songToAdd: number) => {
-  try {
-    const newLikedSong = await prisma.song.update({
-      where: { id: songToAdd },
-      data: {
-        playlists: {
-          connect: {
-            id: 1,
-          },
+  await prisma.song.update({
+    where: { id: songToAdd },
+    data: {
+      playlists: {
+        connect: {
+          id: 1,
         },
       },
-    });
+    },
+  });
 
-    return newLikedSong;
-  } catch (error) {
-    console.error("error setting song to liked playlist", error);
-    throw error;
-  }
+  revalidatePath("/");
 };
 
 export const removeFromLiked = async (songToRemove: number) => {
-  try {
-    const removedSong = await prisma.song.update({
-      where: { id: songToRemove },
-      data: {
-        playlists: {
-          disconnect: { id: 1 },
-        },
+  await prisma.song.update({
+    where: { id: songToRemove },
+    data: {
+      playlists: {
+        disconnect: { id: 1 },
       },
-    });
+    },
+  });
 
-    return removedSong;
-  } catch (error) {
-    console.error("error removing song from liked", error);
-    throw error;
-  }
+  revalidatePath("/");
 };
