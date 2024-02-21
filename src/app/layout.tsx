69068073285e4abef34e19868120314db42b011c
Terence Bezman <terencebezman@gmail.com>
2024-02-20 20:46:22 -0500

    apply circular globally on body

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { circular } from "@/app/fonts";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -13,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={circular.className}>{children}</body>
     </html>
   );
 }
